3b8e9f6f35e0a25bcdc9df133e4924778c3856f3
Ashley2n <a.windbreacker@gmail.com>
2025-10-22 14:33:39 -0500

    Added Database Context Layer to Infrastructure

---
+++ Blog.Domain/Repositories/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace Blog.Domain.Repositories;
+
+public interface IRepository<T> where T : class
+{
+    Task<T> GetById(int id, CancellationToken ct = default);
+    Task<List<T>> GetAll(CancellationToken ct = default);
+    Task<T>AddAsync(T entity, CancellationToken ct = default);
+    Task<T>UpdateAsync(T entity, CancellationToken ct = default);
+    Task<T>DeleteAsync(T entity, CancellationToken ct = default);
+}
---
+++ Blog.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Infrastructure.Data;
+
+public class AppDbContext: DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+    
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder b)
+    {
+        b.Entity<Blog>(e =>
+        {
+            e.HasKey(x => x.Id);
+            e.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+            e.Property(x => x.Title)
+                .IsRequired();
+            // Add additional Configurations Here
+        });
+
+        b.Entity<User>(e =>
+        {
+            e.HasKey(x => x.Id);
+            e.Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+            e.Property(x => x.Name)
+                .IsRequired();
+        });
+    }
+}
