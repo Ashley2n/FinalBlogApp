8d225fdc43560f49eacab7bf7750db36a7adbb7d
Ylyas555 <ylyas.mow96@gmail.com>
2025-10-27 14:55:10 -0500

    first

---
+++ Blog.Application/DTOs/CreateOrderRequest.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace Blog.Application.DTOs;
+
+public class CreateOrderRequest
+{
+    public string Author { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    public DateTime Created { get; set; }
+}
