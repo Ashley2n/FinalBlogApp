1f7fb015c467b52616f79c61f58598dcbfe3f4d2
LaillahZ <159944841+LaillahZ@users.noreply.github.com>
2025-10-28 11:57:48 -0500

    Delete Blog.Application/DTO directory

--- Blog.Application/DTO/BlogPosts/CreateBlogPostDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Blog.Application.DTO.BlogPosts;
-
-public class CreateBlogPostDto
-{
-    public string Author{get;set;}
-    public string Title{get;set;}
-    public string content{get;set;}
-}
